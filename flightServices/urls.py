2ce5243d9f69a9abff4310cff3a9c5a1087fb922
trevin-livele <liveletrevin6@gmail.com>
2022-04-09 22:13:03 +0300

    added url patterns

--- flightServices/urls.py
+++ flightServices/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from flightApp import views
+from rest_framework import DefaultRouter
+
+
+router = DefaultRouter()
+
+router.register('flights',views.FlightViewSet)
+router.register('passengers',views.PassengerViewSet)
+router.register('reservations',views.ReservationViewSet)
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('flightservices/', include(router.urls))
 ]
