64b39c277218404f5b8a41591e86678ed8942a5c
trevin-livele <liveletrevin6@gmail.com>
2022-04-09 21:56:15 +0300

    added models

--- flightApp/models.py
+++ flightApp/models.py
@@ -1,3 +1,23 @@
+import email
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    flightNumber = models.CharField(max_length=10)
+    operatingAirlines = models.CharField(max_length=20)
+    departureCity = models.CharField(max_length=20)
+    arrivalCity = models.CharField(max_length=20)
+    dateOfDeparture = models.DateField()
+    estimatedTimeOfDeparture = models.TimeField()
+
+
+class Passenger(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    middleName = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=10)
+
+class Reservation(models.Model):
+    flight = models.OneToOneField(Flight,ondelete="models.CASCADE")
+    passenger = models.OneToOneField(Passenger,ondelete="models.CASCADE")
